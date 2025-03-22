8eb6cd3d89125aaac3d0d172fd8e83806ff4938f
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-03-22 20:54:51 +0200

    web server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,63 @@
-use std::collections::HashSet;
+use std::sync::Mutex;
 
-use db::{Database, ModQueryOptions};
+use actix_files::NamedFile;
+use actix_web::{
+	App, Either, HttpResponse, HttpServer, Responder, get,
+	http::{Method, StatusCode},
+	web::{self, Data, Html},
+};
+use db::Database;
+use tera::{Context, Tera};
 
 mod db;
 mod mods;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
 	let db = Database::open_connection().unwrap();
-	//refresh_mods(&db, mods::ModRefreshOptions::CacheOnly).unwrap();
-	let ignored_categories = vec!["Suits"].into_iter().map(|s| s.to_string()).collect();
-	let mods = db
-		.get_mods(&ModQueryOptions {
-			ignored_categories,
-			limit: 20,
-			include_deprecated: false,
-			include_nsfw: true,
-		})
-		.unwrap();
+	let data = Data::new(Mutex::new(db));
 
-	for modd in mods {
-		println!("{} - {}", modd.name, modd.description);
+	let port = 3000;
+	println!("Starting server on port {port}");
+
+	HttpServer::new(move || {
+		let tera = Data::new(Tera::new("templates/*.html").unwrap());
+
+		App::new()
+			.app_data(data.clone())
+			.app_data(tera)
+			.service(favicon)
+			.service(welcome_page)
+			.default_service(web::to(default_handler))
+	})
+	.bind(("127.0.0.1", port))?
+	.run()
+	.await
+}
+
+#[get("/")]
+async fn welcome_page(template: Data<Tera>) -> Result<Html, actix_web::Error> {
+	let ctx = Context::new();
+	let html = template
+		.render("index.html", &ctx)
+		.map_err(|_| actix_web::error::ErrorInternalServerError("Template error"))?;
+
+	Ok(Html::new(html))
+}
+
+#[get("/favicon.ico")]
+async fn favicon() -> actix_web::Result<impl Responder> {
+	Ok(NamedFile::open("static/favicon.ico")?)
+}
+
+async fn default_handler(req_method: Method) -> actix_web::Result<impl Responder> {
+	match req_method {
+		Method::GET => {
+			let file = NamedFile::open("static/404.html")?
+				.customize()
+				.with_status(StatusCode::NOT_FOUND);
+			Ok(Either::Left(file))
+		}
+		_ => Ok(Either::Right(HttpResponse::MethodNotAllowed().finish())),
 	}
 }
