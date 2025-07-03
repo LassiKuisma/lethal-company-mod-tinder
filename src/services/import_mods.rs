4d7116055fa1c0a832e53ea851480c16f258c9bf
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-07-03 15:45:22 +0300

    import mods page: display last import date

--- src/services/import_mods.rs
+++ src/services/import_mods.rs
@@ -1,9 +1,17 @@
 use std::sync::Mutex;
 
 use actix_files::NamedFile;
-use actix_web::{Responder, get, post, web::Data};
+use actix_web::{
+	Either, Responder, get, post,
+	web::{Data, Html},
+};
+use tera::{Context, Tera};
+use time::{OffsetDateTime, macros::format_description};
 
-use crate::middlewares::{PrivilegeValidator, TokenValidator};
+use crate::{
+	db::Database,
+	middlewares::{PrivilegeValidator, TokenValidator},
+};
 
 #[derive(Debug, Default, Clone)]
 pub struct ImportStatus {
@@ -12,17 +20,41 @@ pub struct ImportStatus {
 }
 
 #[get("/import-mods", wrap = "PrivilegeValidator", wrap = "TokenValidator")]
-pub async fn import_mods_page(import_status: Data<Mutex<ImportStatus>>) -> impl Responder {
+pub async fn import_mods_page(
+	template: Data<Mutex<Tera>>,
+	db: Data<Database>,
+	import_status: Data<Mutex<ImportStatus>>,
+) -> Result<impl Responder, actix_web::Error> {
 	let import_in_progress = {
 		let import_status = import_status.lock().unwrap();
 		import_status.import_requested || import_status.import_in_progress
 	};
 
 	if import_in_progress {
-		return NamedFile::open("static/import_in_progress.html");
+		return Ok(Either::Left(NamedFile::open(
+			"static/import_in_progress.html",
+		)));
 	}
 
-	NamedFile::open("static/import_mods.html")
+	let mut ctx = Context::new();
+
+	let latest_import = db
+		.latest_mod_import_date()
+		.await
+		.map_err(|_| actix_web::error::ErrorInternalServerError("Database error"))?;
+
+	ctx.insert("latest_import", &latest_import_string(latest_import));
+
+	let html = template
+		.lock()
+		.unwrap()
+		.render("import_mods.html", &ctx)
+		.map_err(|err| {
+			log::error!("{err}");
+			actix_web::error::ErrorInternalServerError("Template error")
+		})?;
+
+	Ok(Either::Right(Html::new(html)))
 }
 
 #[post("/import-mods", wrap = "PrivilegeValidator", wrap = "TokenValidator")]
@@ -30,4 +62,42 @@ pub async fn import_mods(import_status: Data<Mutex<ImportStatus>>) -> impl Respo
 	log::info!("Mod reimport requested");
 	import_status.lock().unwrap().import_requested = true;
 	NamedFile::open("static/import_in_progress.html")
+}
+
+fn latest_import_string(latest_import: Option<OffsetDateTime>) -> String {
+	let date = match latest_import {
+		None => return "Never".to_string(),
+		Some(date) => date,
+	};
+
+	let date_str = date.format(format_description!(
+		"[year]-[month]-[day] [hour]:[minute]UTC"
+	));
+
+	let date_str = match date_str {
+		Ok(str) => str,
+		Err(err) => {
+			log::error!("Error formatting date: {err}");
+			return "---".to_string();
+		}
+	};
+
+	let now = OffsetDateTime::now_utc();
+	let elapsed = now - date;
+
+	let minutes = elapsed.whole_minutes();
+	let hours = elapsed.whole_hours();
+	let days = elapsed.whole_days();
+
+	let time_since = if days > 0 {
+		format!("{days} days ago")
+	} else if hours > 0 {
+		format!("{hours} hours ago")
+	} else if minutes >= 5 {
+		format!("{minutes} minutes ago")
+	} else {
+		"just now".to_string()
+	};
+
+	format!("{date_str} ({time_since})")
 }
